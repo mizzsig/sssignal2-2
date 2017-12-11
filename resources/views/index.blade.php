42ac058cc36b312c1f411dff2c7b667fb9ceadc6
vagrant <vagrant@vagrant.vm>
2017-12-11 21:46:06 +0900

    add top view template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>水飴信号ばーじょん２</title>
-  </head>
-  <body>
-    <div>Mizzsig Presents</div>
-    <div>水飴信号^^</div>
-  </body>
-</html>
+@extends('common.layout')
+
+@section('title', '水飴信号ばーじょん２')
+
+@section('header')@include('common.header')@endsection
+
+@section('main')
+@endsection
+
+@section('sub')@include('common.sub')@endsection
 
