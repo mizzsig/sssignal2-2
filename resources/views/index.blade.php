e4e22fec3baddf2676e6d76d38a1fd7dba0b4797
mizzsig <vagrant@vagrant.vm>
2017-12-24 21:41:48 +0900

    トップページのCSS 記事部分は考え中...

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,8 +8,14 @@
 
 @section('main')
 <div>記事一覧</div>
-<input class="search-bar"></input>
+<input class="search-bar" placeholder="記事を検索"></input>
 <button class="search-button" onClick="console.log(1);">けんさく！</button>
+<!-- dummy -->
+<div>
+	<div class="post">aaa</div>
+	<div class="post">bbb</div>
+	<div class="post">ccc</div>
+</div>
 @endsection
 
 @section('sub')
