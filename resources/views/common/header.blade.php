8710cc03504e3dcf6ab8eea3165d9834dc534424
mizzsig <vagrant@vagrant.vm>
2018-01-07 13:41:59 +0900

    CSS レスポンシブ対応など

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,4 +1,4 @@
-<ul>
+<ul class="large-header">
 	<li><a href="/"><img src="https://sssignal.com/assets/header_mini.png"></a></li>
 	<li><a href="/">Top</a></li>
 	<li><a href="/about">About</a></li>
@@ -7,3 +7,15 @@
 	<li><a href="/column">Column</a></li>
 </ul>
 
+<ul class="small-header">
+	<li><a href="/"><img src="https://sssignal.com/assets/header_mini.png"></a></li>
+	<li class="menu-trigger">menu ▽</li>
+</ul>
+
+<ul class="small-menu">
+	<li><a href="/">Top</a></li>
+  <li><a href="/about">About</a></li>
+  <li><a href="/game">Game</a></li>
+  <li><a href="/soft">Soft</a></li>
+  <li><a href="/column">Column</a></li>
+</ul>
