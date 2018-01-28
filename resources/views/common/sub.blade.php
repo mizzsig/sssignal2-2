9bbbc3ad3042859ffa7da778e77c728a018bd214
mizzsig <vagrant@vagrant.vm>
2018-01-28 21:14:13 +0900

    広告部分はまだ取ってないので消した

--- resources/views/common/sub.blade.php
+++ resources/views/common/sub.blade.php
@@ -1,6 +1,8 @@
+<!-- 今後のバージョンで広告つけます！٩( 'ω' )و
 <div class="google-ad">
 	こうこく
 </div>
+-->
 
 <a class="twitter-timeline" data-height="500" href="https://twitter.com/mizzsig?ref_src=twsrc%5Etfw">Tweets by mizzsig</a> 
 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
