ddfd79fa4c81035b151c333ad615de2520dfb1d2
mizzsig <ihrwon@yahoo.co.jp>
2018-06-05 00:16:07 +0900

    ゲームのmetaタグとtitle調整٩(๑❛ᴗ❛๑)۶

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,13 +1,13 @@
 @extends('common.plane')
 
-@section('title', $post['title'] . ' - 水飴信号 - Starch Syrup Signal -')
+@section('title', $post['title'])
 
 @section('meta')
 @inject('body', 'App\Providers\PostBodyServiceProvider')
 {!! $body->writeStyle($post['styles']) !!}
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@mizzsig">
-<meta name="twitter:title" content="{{ $post['title'] . ' - 水飴信号 - Starch Syrup Signal -' }}">
+<meta name="twitter:title" content="{{ $post['title'] }}">
 <meta name="description" content="{{ $post['description'] }}">
 <meta name="twitter:description" content="{{ $post['description'] }}">
 <meta name="twitter:image" content="{{'https://sssignal.com' . $post['image'] }}">
