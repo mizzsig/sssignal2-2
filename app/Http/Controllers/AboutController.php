1601204cb547dcf47213f11a3018e70e7f6d08ae
mizzsig <ihrwon@yahoo.co.jp>
2018-07-08 23:02:57 +0900

    [mod] インデント直した Pt.2 ('Д')

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -11,22 +11,22 @@ class AboutController extends Controller
 {
 		// route 'about'
     static function index()
-		{
-				return view('about');
+	{
+		return view('about');
+	}
+
+	// お問い合わせメール送信
+	static function contactSent(Request $request)
+	{
+		$contact = $request->all();
+
+		if (!isset($contact['body'])) {
+			return view('about');
 		}
 
-		// お問い合わせメール送信
-		static function contactSent(Request $request)
-		{
-				$contact = $request->all();
+		Mail::to($_ENV['MAIL_USERNAME'])
+			->send(new ContactSent($contact));
 
-				if (!isset($contact['body'])) {
-					return view('about');
-				}
-				
-				Mail::to($_ENV['MAIL_USERNAME'])
-						->send(new ContactSent($contact));
-
-				return view('about');	
-		}
+		return view('about');
+	}
 }
